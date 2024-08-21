7ad5039363aba5dad8e83bbe9450bb231d56e6fd
mohamedhri <125465245+mohamedhri@users.noreply.github.com>
2024-08-21 20:30:48 +0100

    create the springboot envirement + dependencies

---
+++ nurseryFilrouge/src/main/java/com/example/nurseryFilrouge/NurseryFilrougeApplication.java
@@ -0,0 +1,13 @@
+package com.example.nurseryFilrouge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NurseryFilrougeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NurseryFilrougeApplication.class, args);
+	}
+
+}
---
+++ nurseryFilrouge/src/test/java/com/example/nurseryFilrouge/NurseryFilrougeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nurseryFilrouge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NurseryFilrougeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
