c9b068456f34ca7aecb772eb3e1b0229fef69853
mohamedhri <125465245+mohamedhri@users.noreply.github.com>
2024-08-31 17:52:05 +0100

    upadte the avis repo

--- nurseryFilrouge/src/main/java/com/example/nurseryFilrouge/repository/AvisRepository.java
+++ nurseryFilrouge/src/main/java/com/example/nurseryFilrouge/repository/AvisRepository.java
@@ -4,5 +4,9 @@ import com.example.nurseryFilrouge.model.Avis;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface AvisRepository extends JpaRepository<Avis, Long> {}
+public interface AvisRepository extends JpaRepository<Avis, Long> {
+    List<Avis> findByCrecheId(Long crecheId);
+}
